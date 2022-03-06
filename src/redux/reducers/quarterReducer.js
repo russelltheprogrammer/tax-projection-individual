889c3196973f7978ae46a69aef294dccbabf8e89
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-06 12:03:47 -0500

    redux try and fix

--- src/redux/reducers/quarterReducer.js
+++ src/redux/reducers/quarterReducer.js
@@ -2,16 +2,7 @@ const initialState = 1;
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
-        case "1":
-            state = action.payload;
-            break;
-        case "2":
-            state = action.payload;
-            break;
-        case "3":
-            state = action.payload;
-            break;
-        case "4":
+        case "CHANGEQUARTER":
             state = action.payload;
             break;
         default:
