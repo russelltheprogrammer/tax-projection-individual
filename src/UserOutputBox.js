902ce33df87d9cfc9073317136cbb290a810fc99
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-09 17:34:51 -0500

    mess around with output

--- src/UserOutputBox.js
+++ src/UserOutputBox.js
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+
 const UserOutputBox = () => {
+
+const quarter = useSelector((state) => state.quarter);
+
     return ( 
         <div>
-            
+            {quarter}
+            <br/>
+            &nbsp;<br/>
+           
         </div>
      );
 }
