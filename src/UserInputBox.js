d0c21d045e81058f9f96e79e25ce629658eb6f0c
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-11 18:22:12 -0500

    change to map

--- src/UserInputBox.js
+++ src/UserInputBox.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { changeQuarter } from "./redux/action-creators/actionsMaster";
+import { quarterOptionsValues } from "./constants";
 
 const UserInputBox = () => {
 
@@ -11,7 +12,7 @@ const [ quarterState, setQuarterState ] = useState(1);
 
 const handleQuarterSubmit = () => {
     let tempQuarter = parseInt(quarterState);
-    
+
     if([1, 2, 3, 4].indexOf(tempQuarter) > -1){
     dispatch(changeQuarter(tempQuarter));
     setQuarterState(tempQuarter);
@@ -28,17 +29,14 @@ const onQuarterChange = (e) => {
 };
 
     return ( 
-        <div>
+        <div id="input-box">
                 <label htmlFor="quarterinput">Quarter:</label><br/>
                 <select name="quarterinput" id="quarter-input" onChange={onQuarterChange} value={quarterState}>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
+                    {quarterOptionsValues.map((value) => 
+                        <option key={value.id} value={value.quarter}>{value.quarter}</option>
+                    )};
                 </select><br/>
             <button type="submit" onClick={handleQuarterSubmit}>SUBMIT</button><br/>
-            {quarter}<br/>
-            {quarterState}
         </div>
      );
 }
