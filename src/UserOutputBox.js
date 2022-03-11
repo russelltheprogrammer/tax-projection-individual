9019cf289ae1b1316649de7f4be7e26fa7e4e8e4
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-11 18:22:07 -0500

    work on design

--- src/UserOutputBox.js
+++ src/UserOutputBox.js
@@ -5,11 +5,22 @@ const UserOutputBox = () => {
 const quarter = useSelector((state) => state.quarter);
 
     return ( 
-        <div>
-            {quarter}
-            <br/>
-            &nbsp;<br/>
+        <div id="output-box">
+            <div className="d-flex justify-content-start">
+                <div>QUARTER: {quarter}</div>
+                <div>W-2:</div>
+                <div>Interest:</div>
+                <div>Dividends:</div>
            
+                <div>Capital Gains:</div>
+                <div>Business Income:</div>
+                <div>Other Income:</div>
+
+                <div>HSA Contributions:</div>
+                <div>Deductible S/E Tax:</div>
+                <div>S/E Insurance:</div>
+                <div>Retirement Deduction:</div>
+            </div>
         </div>
      );
 }
