e038d60b12ff4a46236678581bcd0e44914089ef
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-11 18:21:56 -0500

    quarter constants

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const quarterOptionsValues = [
+    { id: 0, quarter: 1 },
+    { id: 1, quarter: 2},
+    { id: 2, quarter: 3} ,
+     {id: 3, quarter: 4 }
+    ];
+
+export { quarterOptionsValues };
