6200b061941ffd45153ac9a4f7efe78411462b71
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-15 12:10:21 -0400

    work on styling and design

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
     return ( 
         <div className="header-heading">
             <h1>Tax Projection - Quarterly Estimates</h1>
-            <p>Note: This is for Individuals only, other tax return types will not work.</p>
+            <p>Note: This is for Individuals tax returns only, other tax return types will not work. At this time this projection application can only handle New York State
+                and New York City jurisdictions.
+            </p>
         </div>
      );
 }
--- src/UserOutputBox.js
+++ src/UserOutputBox.js
@@ -1,25 +1,44 @@
 import { useSelector } from "react-redux";
+import { taxInfoElements, taxIncomeElements, taxAdjustmentElements, taxItemizedDeductionElements } from "./constants";
 
 const UserOutputBox = () => {
 
 const quarter = useSelector((state) => state.quarter);
 
     return ( 
-        <div id="output-box">
-            <div className="d-flex justify-content-start">
-                <div>QUARTER: {quarter}</div>
-                <div>W-2:</div>
-                <div>Interest:</div>
-                <div>Dividends:</div>
-           
-                <div>Capital Gains:</div>
-                <div>Business Income:</div>
-                <div>Other Income:</div>
-
-                <div>HSA Contributions:</div>
-                <div>Deductible S/E Tax:</div>
-                <div>S/E Insurance:</div>
-                <div>Retirement Deduction:</div>
+        <div>
+         <div id="output-name-box">
+          <span>NAME:</span>
+          <span>ID:</span>
+          {quarter} 
+         </div>
+         <div id="output-box">
+             <div className="container-fluid">
+                <div className="d-flex align-content-around flex-wrap justify-content-start">
+                    <div>TAX INFO</div>
+                    {taxInfoElements.map((item) =>
+                    <div className="p-3" key={item.id}>{item.element}: </div>
+                    )}
+                </div>
+                <div className="d-flex align-content-around flex-wrap justify-content-start">
+                    <div>INCOME</div>
+                    {taxIncomeElements.map((item) =>
+                    <div className="p-3" key={item.id}>{item.element}: </div>
+                    )}
+                </div>
+                <div className="d-flex align-content-around flex-wrap justify-content-start">
+                    <div>ADJUSTMENT</div>
+                    {taxAdjustmentElements.map((item) =>
+                    <div className="p-3" key={item.id}>{item.element}: </div>
+                    )}
+                </div>
+                <div className="d-flex align-content-around flex-wrap justify-content-start">
+                    <div>DEDUCTIONS</div>
+                    {taxItemizedDeductionElements.map((item) =>
+                    <div className="p-3" key={item.id}>{item.element}: </div>
+                    )}
+                </div>
+             </div>
             </div>
         </div>
      );
