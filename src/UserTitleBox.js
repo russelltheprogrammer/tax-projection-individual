dd1e3d96b9a01d76277b9fe1d3411b914a544019
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-25 17:09:02 -0400

    make ui changes and other

--- src/UserTitleBox.js
+++ src/UserTitleBox.js
@@ -1,14 +1,24 @@
+import React, { useEffect, useRef } from "react";
 
 
 const UserTitleBox = () => {
 
+    const inputEl = useRef(null);
 
-
+    useEffect(()=>{
+        inputEl.current.focus();
+    },[]);
+    
+    
     return ( 
         <div>
          <div id="user-title-box">
-          <span>NAME:</span>
-          <span>ID:</span> 
+             <form>
+                 <label htmlFor="username">NAME:&nbsp;</label>
+                 <input ref={inputEl} type="text" className="user-input-box" id="username" name="username" />
+                 <label htmlFor="userid">ID:&nbsp;</label>
+                 <input type="text" className="user-input-box" id="userid" name="userid" />
+             </form>
          </div>
         </div>
      );
