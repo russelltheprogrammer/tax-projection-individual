d855193d4013312a15206363829f41ed75d9ddb7
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-03-25 17:08:13 -0400

    edits

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,3 @@
-const quarterOptionsValues = [
-    { id: 0, quarter: 1 },
-    { id: 1, quarter: 2 },
-    { id: 2, quarter: 3 },
-    { id: 3, quarter: 4 }
-];
 
 const taxAssumptionsElements = [
     { id: 0, element: "QUARTER", 
