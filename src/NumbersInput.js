6c7769ead82c8d512cd2ecf04035ee50ac1316af
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-04-26 17:28:59 -0400

    buttons

--- src/NumbersInput.js
+++ src/NumbersInput.js
@@ -1,54 +1,35 @@
 import React, { useState } from "react";
-import { taxIncomeElements, taxAdjustmentElements, taxItemizedDeductionElements, taxOtherFederal, taxOtherState } from "./constants";
+import { taxIncomeElements, taxAdjustmentElements, taxItemizedDeductionElements, taxOtherFederal, taxOtherState, numbersInputInitialState } from "./constants";
 import NumbersInputComponent from "./NumbersInputComponent";
 
 
 const NumbersInput = () => {
 
-    // numbersValues within each type of income, adjustment, deduction must match constants.js hardValue
-    const [ numbersValues, setNumbersValues ] = useState({
-        wages: "0",
-        interest: "0",
-        dividends: "0",
-        retirementIncome: "0",
-        capitalIncome: "0",
-        businessIncome: "0",
-        otherIncome: "0",
-        hsa: "0",
-        deductibleSE: "0",
-        insurance: "0",
-        retirementDeduction: "0",
-        studentLoan: "0",
-        otherAdjustments: "0",
-        medical: "0",
-        taxesPaidItemized: "0",
-        interestPaidItemized: "0",
-        charity: "0",
-        otherItemized: "0",
-        qbi: "0",
-        otherFedDeductions: "0",
-        otherFedTaxes: "0",
-        otherStateIncomeDeductions: "0",
-        stateAdditions: "0",
-        stateSubtractions: "0",
-        otherStateTaxes: "0"
-    });
+    const [ numbersValues, setNumbersValues ] = useState(numbersInputInitialState);
 
     const handleInputChange = (e) => {
 
-    const { name, value } = e.target;
-    const oldNumbersValues = numbersValues;
+        const { name, value } = e.target;
+        const oldNumbersValues = numbersValues;
 
-    setNumbersValues({
-        ...oldNumbersValues,
-        [name]: value
-    });
+        setNumbersValues({
+            ...oldNumbersValues,
+            [name]: value
+        });
     };
 
-    // Need to complete
-    const resetNumbersValuesState = () => {
+    const resetNumbersValuesState = () => setNumbersValues({ ...numbersInputInitialState});
 
-    setNumbersValues();
+    const handleSubmit = () => {
+       
+    
+        try {
+          
+        }
+        
+        catch(err) {
+            console.log(err);
+        }
     }
 
     return ( 
@@ -63,8 +44,8 @@ const NumbersInput = () => {
             handleInputChange={handleInputChange} />
             <NumbersInputComponent elementData={taxOtherState} title={"STATE OTHER"} color={"secondary"} numbersValues={numbersValues} 
             handleInputChange={handleInputChange} />
-            <button id="numbers-input-reset-button" onClick={resetNumbersValuesState}>RESET</button>
-            
+             <button id="numbers-input-submit-button" className="submit-button" onClick={handleSubmit} type="submit">SUBMIT</button><br/>
+            <button id="numbers-input-reset-button" className="reset-button" onClick={resetNumbersValuesState}>RESET</button>
         </div>
      );
 }
