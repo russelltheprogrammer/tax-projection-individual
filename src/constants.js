4e1853376811f5edccd55971547bfbd3da4ae3a8
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-04-26 17:28:40 -0400

    add

--- src/constants.js
+++ src/constants.js
@@ -39,6 +39,35 @@ const taxAssumptionsElements = [
     ]}
 ];
 
+// numbersInputInitialState for each description key must match respective constants.js hardValue
+const numbersInputInitialState = {
+    wages: "0",
+    interest: "0",
+    dividends: "0",
+    retirementIncome: "0",
+    capitalIncome: "0",
+    businessIncome: "0",
+    otherIncome: "0",
+    hsa: "0",
+    deductibleSE: "0",
+    insurance: "0",
+    retirementDeduction: "0",
+    studentLoan: "0",
+    otherAdjustments: "0",
+    medical: "0",
+    taxesPaidItemized: "0",
+    interestPaidItemized: "0",
+    charity: "0",
+    otherItemized: "0",
+    qbi: "0",
+    otherFedDeductions: "0",
+    otherFedTaxes: "0",
+    otherStateIncomeDeductions: "0",
+    stateAdditions: "0",
+    stateSubtractions: "0",
+    otherStateTaxes: "0"
+}
+
 const taxIncomeElements = [
     { id: 0, element: "WAGES (W-2)", hardValue: "wages" },
     { id: 1, element: "INTEREST", hardValue: "interest" },
@@ -78,7 +107,6 @@ const taxOtherState = [
     { id: 1, element: "STATE ADDITIONS", hardValue: "stateAdditions" },
     { id: 2, element: "STATE SUBTRACTIONS", hardValue: "stateSubtractions" },
     { id: 3, element: "OTHER STATE TAXES", hardValue: "otherStateTaxes" }
-
 ];
 
-export { taxAssumptionsElements, taxIncomeElements, taxAdjustmentElements, taxItemizedDeductionElements, taxOtherFederal, taxOtherState };
+export { taxAssumptionsElements, taxIncomeElements, taxAdjustmentElements, taxItemizedDeductionElements, taxOtherFederal, taxOtherState, numbersInputInitialState };
