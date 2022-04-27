e9362b939844aac862ab7a2f13fa5a9b5cc84fb0
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-04-27 16:21:01 -0400

    structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from './Header';
 import UserTitleBox from './UserTitleBox';
 import TaxAssumptionsBox from './TaxAssumptionsBox';
 import NumbersInput from './NumbersInput';
+import PaymentsAndCreditsInput from './PaymentsAndCreditsInput';
+import TaxNotes from './TaxNotes';
 import Footer from './Footer';
 
 
@@ -12,6 +14,8 @@ function App() {
        <UserTitleBox />
        <TaxAssumptionsBox />
        <NumbersInput />
+       <PaymentsAndCreditsInput />
+       <TaxNotes />
        <Footer />
     </div>
   );
