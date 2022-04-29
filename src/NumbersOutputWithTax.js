79648bfe909b32d1c81019e0edb2ad5448ebf8c6
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-04-29 10:54:30 -0400

    work on comp

--- src/NumbersOutputWithTax.js
+++ src/NumbersOutputWithTax.js
@@ -5,18 +5,18 @@ const NumbersOuputWithTax = ({ numbersInputValuesState, taxIncomeElements, taxAd
 
     const totalIncome = summationFunction(numbersInputValuesState, taxIncomeElements);
     const totalAdjustments = summationFunction(numbersInputValuesState, taxAdjustmentElements);
-    const adjustedGrossIncome = 0;
+    const adjustedGrossIncome = totalIncome + totalAdjustments;
     const totalItemized = summationFunction(numbersInputValuesState, taxItemizedDeductionElements);
     const totalFederalOther = summationFunction(numbersInputValuesState, taxOtherFederal);
     const totalStateOther = summationFunction(numbersInputValuesState, taxOtherState);
-    const federalTaxableIncome = 0;
-    const stateTaxableIncome = 0;
+    const federalTaxableIncome = adjustedGrossIncome + totalItemized + totalFederalOther;
+    const stateTaxableIncome = adjustedGrossIncome + totalItemized + totalStateOther;
 
     return (
         <div id="numbers-output-with-tax">
             <header className="box-title">INCOME, DEDUCTION, AND TAX SUMMARY</header>
             <table className="table">
-                <thead>
+                <thead className="table-header">
                     <tr>
                         <th scope="col">DESCRIPTION</th>
                         <th scope="col">FEDERAL</th>
@@ -25,80 +25,80 @@ const NumbersOuputWithTax = ({ numbersInputValuesState, taxIncomeElements, taxAd
                 </thead>
                 <tbody>
                         {taxIncomeElements.map((item) => 
-                        <tr>
-                        <th scope="row" key={item.id}>{item.element}</th>
+                        <tr key={item.id}>
+                        <th scope="row" className="table-description-item">{item.element}</th>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         </tr>
                         )}
 
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">TOTAL INCOME</th>
                         <td>{totalIncome}</td>
                         <td>{totalIncome}</td>
                         </tr>
 
                         {taxAdjustmentElements.map((item) => 
-                        <tr>
-                        <th scope="row" key={item.id}>{item.element}</th>
+                        <tr key={item.id}>
+                        <th scope="row" className="table-description-item">{item.element}</th>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         </tr>
                         )}
 
-                        <tr>
-                        <th scope="row">TOTAL ADJUSTMENTS</th>
+                        <tr className="table-total-row">
+                        <th scope="row" >TOTAL ADJUSTMENTS</th>
                         <td>{totalAdjustments}</td>
                         <td>{totalAdjustments}</td>
                         </tr>
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">ADJUSTED GROSS INCOME</th>
                         <td>{adjustedGrossIncome}</td>
                         <td>{adjustedGrossIncome}</td>
                         </tr>
 
                          {taxItemizedDeductionElements.map((item) => 
-                        <tr>
-                        <th scope="row" key={item.id}>{item.element}</th>
+                        <tr key={item.id}>
+                        <th scope="row" className="table-description-item">{item.element}</th>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         </tr>
                         )}
 
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">TOTAL ITEMIZED</th>
                         <td>{totalItemized}</td>
                         <td>{totalItemized}</td>
                         </tr>
 
                          {taxOtherFederal.map((item) => 
-                        <tr>
-                        <th scope="row" key={item.id}>{item.element}</th>
+                        <tr key={item.id}>
+                        <th scope="row" className="table-description-item">{item.element}</th>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         <td>N/A</td>
                         </tr>
                         )}
 
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">TOTAL FEDERAL OTHER</th>
                         <td>{totalFederalOther}</td>
                         <td>N/A</td>
                         </tr>
 
                          {taxOtherState.map((item) => 
-                        <tr>
-                        <th scope="row" key={item.id}>{item.element}</th>
+                        <tr key={item.id}>
+                        <th scope="row" className="table-description-item">{item.element}</th>
                         <td>N/A</td>
                         <td>{numbersInputValuesState[item.hardValue]}</td>
                         </tr>
                         )}
 
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">TOTAL STATE OTHER</th>
                         <td>N/A</td>
                         <td>{totalStateOther}</td>
                         </tr>
-                        <tr>
+                        <tr className="table-total-row">
                         <th scope="row">TAXABLE INCOME</th>
                         <td>{federalTaxableIncome}</td>
                         <td>{stateTaxableIncome}</td>
