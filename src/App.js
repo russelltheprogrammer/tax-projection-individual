a840e35841a4f6b95460bc2d6a44e5348ba6659a
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-04-29 14:56:06 -0400

    change structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from './Header';
 import UserTitleBox from './UserTitleBox';
 import TaxAssumptionsBox from './TaxAssumptionsBox';
 import NumbersInput from './NumbersInput';
-import PaymentsAndCreditsInput from './PaymentsAndCreditsInput';
 import TaxNotes from './TaxNotes';
 import Footer from './Footer';
 
@@ -14,7 +13,6 @@ function App() {
        <UserTitleBox />
        <TaxAssumptionsBox />
        <NumbersInput />
-       <PaymentsAndCreditsInput />
        <TaxNotes />
        <Footer />
     </div>
