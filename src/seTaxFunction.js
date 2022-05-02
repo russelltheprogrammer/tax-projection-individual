cb9f3f192e42b3119d0280a4f438cc982d89c9dc
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-02 16:58:00 -0400

    add function

---
+++ src/seTaxFunction.js
@@ -0,0 +1,31 @@
+import { MaxSETaxThreshold } from "./constants";
+
+const seTaxFunction = (businessIncome, wages) => {
+    let businessIncomeParsed = parseInt(businessIncome);
+    let wagesParsed = parseInt(wages);
+    let netBusinessIncome = businessIncomeParsed * .9235;
+    let wagesLessMaxSETaxThreshold = MaxSETaxThreshold - wagesParsed;
+    let seTax = 0;
+    let addlSETax = 0;
+
+    if( netBusinessIncome < 400 ) {
+        return seTax;
+    }
+
+    else if (wagesLessMaxSETaxThreshold <= 0) {
+        seTax = netBusinessIncome * .029;
+    }
+    else if (wagesLessMaxSETaxThreshold > 0) {
+        if(netBusinessIncome <= wagesLessMaxSETaxThreshold) {
+        seTax = netBusinessIncome * .153;
+        }
+        else{
+            seTax = wagesLessMaxSETaxThreshold * .124;
+            addlSETax = netBusinessIncome * .029;
+            seTax += addlSETax;
+        }
+    }
+    return Math.round(seTax);
+}
+
+export default seTaxFunction;
