5b855792518be8b835e96b5155c9ef6c83d5954f
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-12 15:59:36 -0400

    add link

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,8 @@
 const Footer = () => {
     return ( 
         <div id="footer">
-            Developed And Built By <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a>
+            Developed And Built By <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
+            Need Tax Help? Contact <a href="https://monteithtaxcpa.herokuapp.com/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA PLLC</b></a> Today For A Free Consultation.
         </div>
      );
 }
