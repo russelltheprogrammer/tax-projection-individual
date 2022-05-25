42c17ef45602680a389cb292d08e6c4f5af10f97
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-25 16:27:42 -0400

    new function

--- src/functions/federalItemizedCalcFunction.js
+++ src/functions/federalItemizedCalcFunction.js
@@ -1,25 +1,12 @@
+import { addTotalNumbersForItemized } from "./addTotalNumbersForItemized";
+
 const  federalItemizedCalcFunction = (agi, values) => {
    let agiToBeLimited = agi; //value to possibly use in the future for a more advance itemized deduction function
 
-   let total = addTotal(values["medical"], values["taxesPaidItemized"], values["interestPaidItemized"], values["charity"], 
+   let total = addTotalNumbersForItemized(values["medical"], values["taxesPaidItemized"], values["interestPaidItemized"], values["charity"], 
    values["otherItemized"]);
   
    return total;
 }
  
 export default federalItemizedCalcFunction;
-
-
-function addTotal() {
-    let array = [];
-    let sum = 0;
-
-    for (let i = 0; i < arguments.length; i++) {
-        array.push(arguments[i]);
-    }
-
-    sum = array.reduce((prevValue, currentValue) => {
-        return prevValue + currentValue
-    });
-    return parseInt(sum);
-};
