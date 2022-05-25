e7dc2c1dfef34470cdcefe0d4b34d12fd020700f
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-25 16:27:05 -0400

    add in new line

--- src/components/ItemizedOutput.js
+++ src/components/ItemizedOutput.js
@@ -1,4 +1,4 @@
-const ItemizedOutput = ({ taxItemizedDeductionElements, taxInputDataAnnualized, totalFederalItemized, totalStateItemized }) => {
+const ItemizedOutput = ({ taxItemizedDeductionElements, taxStateItemizedDeductionElements, taxInputDataAnnualized, totalFederalItemized, totalStateItemized, NA }) => {
 
     return (
             <tbody style={{borderTop: "none" }}>
@@ -9,6 +9,13 @@ const ItemizedOutput = ({ taxItemizedDeductionElements, taxInputDataAnnualized, 
                     <td>{taxInputDataAnnualized[item.hardValue]}</td>
                 </tr>
                 )}
+
+                <tr>
+                    <th scope="row" className="table-description-item">{taxStateItemizedDeductionElements[0]["element"]}</th>
+                    <td className="table-na-section">{NA}</td>
+                    <td>{taxInputDataAnnualized["stateSpecificItemized"]}</td>
+                </tr>
+
 
                 <tr className="table-total-row">
                     <th scope="row">TOTAL ITEMIZED</th>
