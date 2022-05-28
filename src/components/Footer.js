4b582d8da712f402c58401a8ef9277f269fff450
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-05-28 19:20:53 -0400

    change wording

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return ( 
         <div id="footer">
-            Developed And Built By <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
-            Need Tax Help? Contact <a href="https://monteithtaxcpa.herokuapp.com/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA PLLC</b></a> Today For A Free Consultation.
+            Developed and built by <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
+            Need tax help? Contact <a href="https://monteithtaxcpa.herokuapp.com/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA PLLC</b></a> today for a free consultation.
         </div>
      );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 const Header = () => {
     return ( 
         <div className="header-heading">
-            <h1>Tax Projection - Quarterly Estimates</h1>
-            <p>Note: This is for Individuals tax returns only, other tax return types will not work. At this time this projection application can only handle New York State
-                and New York City jurisdictions.
+            <h1>Tax Projection - Quarterly Estimates &#40;BETA&#41;</h1>
+            <p>Note: This is for Individual tax return types only, other tax return types will not work. At this time, this projection application can only handle New York State
+                and New York City tax jurisdictions. Please see contact information below if you have any questions, feature requests, and/or to report any bugs.
             </p>
         </div>
      );
