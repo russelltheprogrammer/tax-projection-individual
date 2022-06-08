80437fa4420990fa3126b6f0f8a9890ea2ab6b96
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-08 15:07:56 -0400

    work on func

--- src/functions/taxCalcFunction.js
+++ src/functions/taxCalcFunction.js
@@ -4,6 +4,7 @@ import { federalSingleTaxBrackets, federalMFJTaxBrackets, federalMFSTaxBrackets,
     from "../constants";
 import taxBracketTaxFunction from "./taxBracketTaxFunction";
 import schDTaxWorksheetLineThresholdCalculation from "./schDTaxWorksheetLineThresholdCalculation";
+import enterSmallerNumberFunction from "./enterSmallerNumberFunction";
 
 const taxCalcFunction = (taxableIncome, filingStatus, residency, shortTermCapital, longTermCapital, qualifiedDividends) => {
    
@@ -13,15 +14,32 @@ const taxCalcFunction = (taxableIncome, filingStatus, residency, shortTermCapita
     let calculatedTax = 0;
 
     // refactor capital gains into its own function?
-    let schDLineSixThreshold = schDTaxWorksheetLineThresholdCalculation(filingStatus, "SchDLineSix");
-    let schDLineThirteenThreshold = schDTaxWorksheetLineThresholdCalculation(filingStatus, "SchDLineThirteen");
+    // Sch D Line 1 is taxableIncome
+    // Sch D Line 2 is qualifiedDividends
     let totalCapitalGains = shortTermCapital + longTermCapital;
-    let SchDLineThree = 0;
-    let capitalGainsPlusQualifiedDividends = 0;
-    let schDLineFive = 0;
+    let schDLineThree = 0;
+    let capitalGainsPlusQualifiedDividends = 0; // Sch D Line 4
+    let taxableIncomeLessQualDivAndCapGains = 0; // Sch D Line 5
+    let schDLineSixThreshold = schDTaxWorksheetLineThresholdCalculation(filingStatus, "SchDLineSix");
     let schDLineSeven = 0;
     let schDLineEight = 0;
     let schDLineNine = 0;
+    let schDLineTen = 0;
+    let schDLineEleven = schDLineNine;
+    let schDLineTwelve = 0;
+    let schDLineThirteenThreshold = schDTaxWorksheetLineThresholdCalculation(filingStatus, "SchDLineThirteen");
+    let schDLineFourteen = 0;
+    let schDLineFifteen = 0;
+    let schDLineSixteen = 0;
+    let schDLineSeventeen = 0;
+    let schDLineEighteen = 0;
+    let schDLineNineteen = 0;
+    let schDLineTwenty = 0;
+    let schDLineTwentyOne = 0;
+    let calculatedTaxOfTaxableIncomeLessQualDivAndCapGains = 0; // Schd D Line 22
+    let totalCapitalGainsTax = 0; // Sch D Line 23 Part 1
+    let totalCalculatedTaxBenefittedFromCapGainsRate = 0; // Sch D Line 23 Part 2 TOTAL
+
 
     if(taxableIncome < 1){
         return calculatedTax;
@@ -29,48 +47,53 @@ const taxCalcFunction = (taxableIncome, filingStatus, residency, shortTermCapita
     else if(residency === "NONE"){
         return calculatedTax;
     }
-    else {
+    else if(residency === "FEDERAL") {
         if(longTermCapital >= totalCapitalGains){
             if(totalCapitalGains <= 0){
-                SchDLineThree = 0;
+                schDLineThree = 0;
             }
             else {
-                SchDLineThree = totalCapitalGains;
+                schDLineThree = totalCapitalGains;
             }
         }
         else {
             if(longTermCapital <= 0){
-                SchDLineThree = 0;
+                schDLineThree = 0;
             }
             else {
-                SchDLineThree = longTermCapital;
+                schDLineThree = longTermCapital;
             }
         }
-    }
-
-    capitalGainsPlusQualifiedDividends += SchDLineThree + qualifiedDividends;
-    schDLineFive = taxableIncome - capitalGainsPlusQualifiedDividends;
-
-    if(schDLineSixThreshold <= taxableIncome){
-        schDLineSeven = schDLineSixThreshold;
-    }
-    else {
-        schDLineSeven = taxableIncome;
-    }
-
-    if(schDLineFive <=  schDLineSeven){
-        schDLineEight = schDLineFive;
-    }
-    else {
-        schDLineEight = schDLineSeven;
-    }
-
-   schDLineNine = schDLineSeven - schDLineEight;
-
     
+    capitalGainsPlusQualifiedDividends += schDLineThree + qualifiedDividends;
+    taxableIncomeLessQualDivAndCapGains = taxableIncome - capitalGainsPlusQualifiedDividends;
+    schDLineSeven = enterSmallerNumberFunction(schDLineSixThreshold, taxableIncome);
+    schDLineEight = enterSmallerNumberFunction(taxableIncomeLessQualDivAndCapGains, schDLineSeven);
+    schDLineNine = schDLineSeven - schDLineEight;
+    schDLineTen = enterSmallerNumberFunction(taxableIncome, capitalGainsPlusQualifiedDividends);
+    schDLineTwelve = schDLineTen - schDLineEleven;
+    schDLineFourteen = enterSmallerNumberFunction(taxableIncome, schDLineThirteenThreshold);
+    schDLineFifteen += taxableIncomeLessQualDivAndCapGains + schDLineNine;
+    schDLineSixteen = schDLineFourteen - schDLineFifteen;
 
+    if(schDLineSixteen < 1){
+        schDLineSixteen = 0;
+    }
+    // else{
+    //     console.log("no Sch D Line Sixteen adjustment needed");
+    // }
+
+    schDLineSeventeen = enterSmallerNumberFunction(schDLineTwelve, schDLineSixteen);
+    schDLineEighteen = Math.round(schDLineSeventeen *.15);
+    schDLineNineteen += schDLineNine + schDLineSeventeen;
+    schDLineTwenty = schDLineTen - schDLineNineteen;
+    schDLineTwentyOne = Math.round(schDLineTwenty *.20);
+    totalCapitalGainsTax += schDLineEighteen + schDLineTwentyOne;
+    }
+    // else{
+    //     console.log("proceed with tax caculation function without cap gains tax rate");
+    // }
    
-
         switch(expr) {
             case "SINGLE & FEDERAL":
                 taxBracket = federalSingleTaxBrackets;
@@ -116,13 +139,29 @@ const taxCalcFunction = (taxableIncome, filingStatus, residency, shortTermCapita
                 return console.log("no state selected, there is an erorr in the taxCalcfunction");
         }
 
-    calculatedTax += taxBracketTaxFunction(taxBracket, taxableIncome, calculatedTax);
+    calculatedTax += taxBracketTaxFunction(taxBracket, taxableIncome);
+
+    if(residency === "FEDERAL"){
+        calculatedTaxOfTaxableIncomeLessQualDivAndCapGains += taxBracketTaxFunction(taxBracket, taxableIncomeLessQualDivAndCapGains);
+    }
+    // else {
+    //     console.log("Federal residency not selected");
+    // }
+
+    totalCalculatedTaxBenefittedFromCapGainsRate += totalCapitalGainsTax + calculatedTaxOfTaxableIncomeLessQualDivAndCapGains;
    
+    if(residency === "FEDERAL"){
+        calculatedTax = enterSmallerNumberFunction(calculatedTax, totalCalculatedTaxBenefittedFromCapGainsRate);
+    }
+    // else{
+    //     console.log("Federal residency not selected");
+    // }
+
     if(taxBracket2 === null){
         return calculatedTax;
     }
     else if(taxBracket2 !== null){
-        calculatedTax += taxBracketTaxFunction(taxBracket2, taxableIncome, calculatedTax);
+        calculatedTax += taxBracketTaxFunction(taxBracket2, taxableIncome);
         return calculatedTax;
     }
     return console.log("No tax calculated, there is an error in the the taxCalcFunction");
