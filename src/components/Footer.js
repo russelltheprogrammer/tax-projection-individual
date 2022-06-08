5853ccd96515884edafcad537e8ea4fe0f74c4b5
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-08 15:05:32 -0400

    change link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return ( 
         <div id="footer">
             Developed and built by <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
-            Need tax help? Contact <a href="https://monteithtaxcpa.herokuapp.com/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA PLLC</b></a> today for a free consultation.
+            Need tax help? Contact <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA PLLC</b></a> today for a free consultation.
         </div>
      );
 }
