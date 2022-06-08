cfbc572a4a2a0369db4e326075924a9ef64e8dc4
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-08 15:05:38 -0400

    add func

---
+++ src/functions/enterSmallerNumberFunction.js
@@ -0,0 +1,12 @@
+const  enterSmallerNumberFunction = (number1, number2) => {
+   
+    if(number1 <= number2){
+        return number1;
+    }
+    else {
+        return number2;
+    }
+
+ }
+  
+ export default enterSmallerNumberFunction;
