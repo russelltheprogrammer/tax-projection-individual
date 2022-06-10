3d58b6b244b59b68a90252432bd817701fb733a5
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-06-10 15:07:49 -0400

    responsiveness

--- src/components/UserTitleBox.js
+++ src/components/UserTitleBox.js
@@ -1,14 +1,24 @@
-import React, { useEffect, useRef } from "react";
-
+import React, { useEffect, useRef, useState } from "react";
 
 const UserTitleBox = () => {
 
+    const [ width, setWidth ] = useState(window.innerWidth);
+    const breakPoint = 1100;
+
     const inputEl = useRef(null);
+
+    useEffect(() => {
+        const handleResizeWindow = () => setWidth(window.innerWidth);
+
+        window.addEventListener("resize", handleResizeWindow);
+        return () => {
+            window.removeEventListener("resize", handleResizeWindow);
+        }
+    }, []);
 
     useEffect(()=>{
         inputEl.current.focus();
     },[]);
-    
     
     return ( 
         <div>
@@ -16,6 +26,7 @@ const UserTitleBox = () => {
              <form>
                  <label htmlFor="username">NAME:&nbsp;</label>
                  <input ref={inputEl} type="text" className="user-input-box" id="username" name="username" />
+                 { width < breakPoint ? <br/> : null }
                  <label htmlFor="userid">ID:&nbsp;</label>
                  <input type="text" className="user-input-box" id="userid" name="userid" />
              </form>
