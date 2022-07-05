7a8cde7946b45bc132dd0742801875c86327d4d5
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-05 17:53:48 -0400

    add tech doc link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ const Header = () => {
         <div className="header-heading">
             <h1>Tax Projection - Quarterly Estimates &#40;BETA&#41;</h1>
             <p>Note: This is for Individual tax return types only, other tax return types will not work. At this time, this projection application can only handle New York State
-                and New York City tax jurisdictions. Please see contact information below if you have any questions, feature requests, and/or to report any bugs.
+                and New York City tax jurisdictions. Please see contact information below if you have any questions, feature requests, and/or to report any bugs.<br/>
+                You can find the technical documentation <a href="https://github.com/russelltheprogrammer/tax-projection-individual" target="_blank" rel="noreferrer"><b>HERE</b></a>
             </p>
         </div>
      );
