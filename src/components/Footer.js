9d9b4e435cee7c63a49492631efadd6f4e929f38
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-05 18:02:18 -0400

    add email contact info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,9 @@ const Footer = () => {
     return ( 
         <div id="footer">
             Developed and built by <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
-            Need tax help? Contact <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA</b></a> today for a free consultation.
+            Need tax help? Contact <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA</b></a> today 
+            for a free consultation.<br/>
+            Send me an <a href ="mailto: russelltheprogrammer@gmail.com"><b>E-MAIL</b></a> to report any bugs, feature requests, and/or questions.
         </div>
      );
 }
