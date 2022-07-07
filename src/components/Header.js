0f6b12bc838516791cb57a809e7769d89d3cae09
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-07 12:28:08 -0400

    work ux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,28 @@
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material/';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
 const Header = () => {
     return ( 
+        // <div>
+        // <Box>
+        //     <AppBar component="nav">
+        //         <Toolbar>
+        //         <IconButton
+        //             color="inherit"
+        //             aria-label="open drawer"
+        //             edge="start"
+        //             // onClick={handleDrawerToggle}
+        //             sx={{ mr: 2, display: { sm: 'none' } }}
+        //             >
+        //             <MenuIcon />
+        //              </IconButton>
+        //         </Toolbar>
+        //     </AppBar>
+
+        // </Box>
+        // </div>
+
         <div className="header-heading">
             <h1>Tax Projection - Quarterly Estimates &#40;BETA&#41;</h1>
             <p>Note: This is for Individual tax return types only, other tax return types will not work. At this time, this projection application can only handle New York State
