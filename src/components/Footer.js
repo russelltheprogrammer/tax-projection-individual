32da025e51b422a3c2d32c20c118e3c023c3d805
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-19 18:05:09 -0400

    make some tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 const Footer = () => {
     return ( 
         <div id="footer">
+            <h2> CONTACT INFORMATION</h2>
             Developed and built by <a href="https://russelltheprogrammer.github.io/personal-webpage/" target="_blank" rel="noreferrer"><b>Russell Monteith</b></a><br/>
             Need tax help? Contact <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA</b></a> today 
             for a free consultation.<br/>
