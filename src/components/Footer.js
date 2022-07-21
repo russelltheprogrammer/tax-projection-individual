731fbfea2d92f75e38f3ac907168e596190e8133
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-21 16:15:19 -0400

    redesign message

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,8 @@ const Footer = () => {
             Need tax help? Contact <a href="https://www.linkedin.com/in/russell-monteith-cpa-0a43975a/" target="_blank" rel="noreferrer"><b>Russell Monteith CPA</b></a> today 
             for a free consultation.<br/>
             Send me an <a href ="mailto: russelltheprogrammer@gmail.com"><b>E-MAIL</b></a> to report any bugs, feature requests, and/or questions.
+            <br/><span style={{color: "red", fontWeight: "bold"}}>
+                This application is currently undergoing a redesign. Thank you for your understanding.</span>
         </div>
      );
 }
