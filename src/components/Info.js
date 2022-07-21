2b452b87b03db86e4c80997dbcc7383aa894e1d1
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-21 16:15:38 -0400

    add info section

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+const Info = () => {
+    return (  
+        <div id="info">
+            <h2>INFORMATION</h2>
+            <p>Note: This is for Individual tax return types only, other tax return types will not work. At this time, this projection application can only handle New York State
+                and New York City tax jurisdictions. Please see contact information below if you have any questions, feature requests, and/or to report any bugs.
+                Please check out the <a href="https://github.com/russelltheprogrammer/tax-projection-individual#readme" target="_blank" rel="noreferrer"><b>technical documentation </b></a>
+                for additional information.
+            </p>
+        </div>
+      )
+}
+ 
+export default Info;
