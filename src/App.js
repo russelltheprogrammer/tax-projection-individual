059895f1ddfcb523a103385c66e766a68092b2ec
russelltheprogrammer <russelltheprogrammer@gmail.com>
2022-07-21 16:14:48 -0400

    add info section

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import TaxAssumptionsBox from './components/TaxAssumptionsBox';
 import NumbersInput from './components/NumbersInput';
 import TaxNotes from './components/TaxNotes';
 import Footer from './components/Footer';
+import Info from './components/Info'
 
 
 function App() {
   return (
     <div className="App">
-       <Header/>
-       <UserTitleBox />
-       <TaxAssumptionsBox />
-       <NumbersInput />
-       <TaxNotes />
-       <Footer />
+        <Header/>
+        <UserTitleBox />
+        <TaxAssumptionsBox />
+        <NumbersInput />
+        <TaxNotes />
+        <Info />
+        <Footer />
     </div>
   );
 }
